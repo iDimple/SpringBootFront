2db51f6148ab08610bad21ca78f95a81f4f3d665
iDimple <wuqinyue@sjtu.edu.cn>
2019-05-23 19:57:42 +0800

    增加展开折叠的图标

--- SpringBootFront/src/main/resources/META-INF/resources/mrc.js
+++ SpringBootFront/src/main/resources/META-INF/resources/mrc.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
 
 
-var entity_url="";
-var jsonShow=[];
-indexOfClick=1;
-clickAnswer=function(i){
-	console.log(i);
-	console.log(entity_url);
-	console.log(jsonShow);
-	$("#entity"+i).toggle();
-}
+	var entity_url="";
+	var jsonShow=[];
+	indexOfClick=1;
+	clickAnswer=function(i){
+		console.log(i);
+		console.log(entity_url);
+		console.log(jsonShow);
+		$("#entity"+i).toggle();
+		if($("#entitydetail"+i).attr("src")=="assets/img/fold.png"){
+			$("#entitydetail"+i).attr("src","assets/img/unfold.png");
+			$("#entitydetail"+i).attr("title","more details");
+		}else{
+			$("#entitydetail"+i).attr("src","assets/img/fold.png");
+			$("#entitydetail"+i).attr("title","fold the details");
+		}
+	}
 
 
 	$("#st-input-btn").click(function(){
@@ -46,7 +53,7 @@ clickAnswer=function(i){
 					} else {
 						jsonShow=json[0].entity;
 //						$("#answer").append('<div class="st-message-wrap answer"><div class="st-message-icon"></div><div class="st-message-r"></div><div class="st-message"><div class="st-outline">'
-//								+"To best of my knowledge, below is the answer~");
+//						+"To best of my knowledge, below is the answer~");
 //						$("#answer").append('</div></div></div><div style="height:150px"></div>');	
 						var dataJson=json[0];
 						var context=dataJson.context;
@@ -55,26 +62,30 @@ clickAnswer=function(i){
 						entity_url=jsonShow.Id;
 						for(var i=0;i<context.length;i++){
 							if(i==(dataJson.answer_start)){
-								
+
 								showContext+='<a onclick=clickAnswer('+indexOfClick+') style="color:red;cursor:pointer">';
 								console.log(showContext)
-								
-							}
-							if(i==(dataJson.answer_end)){
-								showContext+='</a>';
-								console.log(i)
+
 							}
 							showContext+=context[i];
+							if(i==(dataJson.answer_end)){
+								showContext+="<img src='assets/img/unfold.png' alt='more details' title='more details' id='entitydetail"+indexOfClick+"'></img>";
+								showContext+='</a>';
+								console.log(i)
+
+							}
+						
 						}
 						showContext+="</div>";
+
 						$("#answer").append('<div>'+showContext+'</div></div></div>');
 						$("#answer").append("<div style='clear:both;margin-bottom:20px;'></div>");
 						//entity
-						
+
 						for (var i = 0; i < jsonShow.length; ++i) {
 							console.log(i);
 							displayEntity(jsonShow[i]);
-							
+
 //							$("#answer").append("<hr style='border: 1px solid lightgray;'>");
 							$("#entity"+indexOfClick).css("padding-left","60px");
 							$("#entity"+indexOfClick).hide();
@@ -82,11 +93,11 @@ clickAnswer=function(i){
 						}
 
 						Prism.highlightAll();
-					
-					
+
+
 					}
 //					if(he>499){
-//						$("#st-message-box").scrollTop(he);
+//					$("#st-message-box").scrollTop(he);
 //					}
 				} else {
 					$("#answer").append("<p>" + result.error_msg + "</p>");
