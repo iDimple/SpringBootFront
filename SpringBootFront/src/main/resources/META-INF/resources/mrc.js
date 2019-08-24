0c5549803d0c2aeb5d401e23123ef8931a10e2c4
iDimple <wqy14@software.nju.edu.cn>
2019-08-24 17:28:16 +0800

    空格不转义

--- SpringBootFront/src/main/resources/META-INF/resources/mrc.js
+++ SpringBootFront/src/main/resources/META-INF/resources/mrc.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 		temp = str.replace(/&/g,"&amp;");
 		temp = temp.replace(/</g,"&lt;");
 		temp = temp.replace(/>/g,"&gt;");
-		temp = temp.replace(/\s/g,"&nbsp;");
+//		temp = temp.replace(/\s/g,"&nbsp;");
 		temp = temp.replace(/\'/g,"&#39;");
 		temp = temp.replace(/\"/g,"&quot;");
 		console.log(temp);
@@ -21,7 +21,7 @@ $(document).ready(function () {
 		temp = str.replace(/&amp;/g,"&");
 		temp = temp.replace(/&lt;/g,"<");
 		temp = temp.replace(/&gt;/g,">");
-		temp = temp.replace(/&nbsp;/g," ");
+//		temp = temp.replace(/&nbsp;/g," ");
 		temp = temp.replace(/&#39;/g,"\'");
 		temp = temp.replace(/&quot;/g,"\"");
 		return temp;  
